a81353ba4d64826a7edd6b7f8e8e7df743111a8a
Bernd Helmle <bernd@oopsware.de>
2025-06-18 12:36:34 +0200

    Rename include defines.
    The still have the old project name, rename them to reflect new naming.
    
    Issue #1, by @ivan.kovmir

--- pgxcrypto_argon2.h
+++ pgxcrypto_argon2.h
@@ -2,12 +2,12 @@
 // Created by bernd on 13.02.25.
 //
 
-#ifndef PGXCRYPTO_POC_PGXCRYPTO_ARGON2_HXX
-#define PGXCRYPTO_POC_PGXCRYPTO_ARGON2_HXX
+#ifndef PGXCRYPTO_PWHASH_PGXCRYPTO_ARGON2_HXX
+#define PGXCRYPTO_PWHASH_PGXCRYPTO_ARGON2_HXX
 
 StringInfo xgen_salt_argon2(Datum *options, int numoptions, const char *magic);
 
 Datum
 xcrypt_argon2(Datum password, Datum salt);
 
-#endif //PGXCRYPTO_POC_PGXCRYPTO_ARGON2_HXX
+#endif
--- pgxcrypto_yescrypt.h
+++ pgxcrypto_yescrypt.h
@@ -1,5 +1,5 @@
-#ifndef PGXCRYPTO_POC_PGXCRYPT_YESCRYPT_H
-#define PGXCRYPTO_POC_PGXCRYPT_YESCRYPT_H
+#ifndef PGXCRYPTO_PWHASH_PGXCRYPT_YESCRYPT_H
+#define PGXCRYPTO_PWHASH_PGXCRYPT_YESCRYPT_H
 
 #include "postgres.h"
 #include "pgxcrypto_pwhash.h"
@@ -10,5 +10,4 @@ xgen_salt_yescrypt(Datum *options, int numoptions, const char *magic);
 Datum
 xcrypt_yescrypt_crypt(Datum password, Datum salt);
 
-#endif //PGXCRYPTO_POC_PGXCRYPT_YESCRYPT_H
-
+#endif
